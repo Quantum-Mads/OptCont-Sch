ed3e3375bbf53451d6ba4701cef52e0ddb444414
calvarez <calvarez@quantum-mads.com>
2025-09-04 12:05:50 +0200

    Create SRI-model_Classical_HS.py

---
+++ SRI-model/SRI-model_Classical_HS.py
@@ -0,0 +1,8 @@
+"""
+SIR Model: The SIR model is a simple and widely used mathematical model to describe the spread of infectious diseases in a population. It divides the population into three compartments:
+
+- S: Susceptible -> Number of susceptible individuals who can catch the disease.
+- I: Infected -> Number of individuals who have the disease and can spread it to susceptible individuals.
+- R: Recovered -> Number of individuals who have recovered from the disease and are now immune.
+
+"""
